e63960df69fea055feb87c9572b8ad737f1e7b22
Brandon Q <bqdemon@gmail.com>
2014-07-12 14:36:18 +1000

    A website to present all code and design work

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DCOTYPE html>
+
+<html>
+	
+	<head>
+		<title>Brandon Quinn</title>
+
+		<link href='http://fonts.googleapis.com/css?family=EB+Garamond|Oswald' rel='stylesheet' type='text/css'>
+
+		<link rel="stylesheet" href="css/normalize.css" type="text/css"/>
+		<link rel="stylesheet" href="css/template.css" type="text/css"/>
+		<link rel="stylesheet" href="css/navbar.css" type="text/css"/>
+		
+	</head>
+
+	<body>
+		
+		<div id="wrapper">
+
+			<div id="navbar">
+				<div id="navbar_stuff">
+					<ul id="nav_left">
+						<a href="#"><li>HOME</li></a>
+						<a href="#"><li>ARTICLES</li></a>
+					</ul>
+						<a href="#"><img id="flag_banner" src="res/flag.png"></a>
+					<ul id="nav_right">
+						<a href="#"><li>WORK</li></a>
+						<a href="#"><li>CONTACT</li></a>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+	</body>
+</html>
